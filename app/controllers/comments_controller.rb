82029912810ec515a3aaf9143c6270e9aed56b5b
kterhar-edu <81818592+kterhar-edu@users.noreply.github.com>
2024-04-09 13:23:43 +0000

    added authorized user to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
+  before_action :is_an_authorized_user, only: [:destroy, :create]
 
   # GET /comments or /comments.json
   def index
@@ -63,8 +64,16 @@ class CommentsController < ApplicationController
       @comment = Comment.find(params[:id])
     end
 
+    def is_an_authorized_user
+      @photo = Photo.find(params.fetch(:comment).fetch(:photo_id))
+      #marks as not authorized if its not their own picture AND the photo owner is private AND the user doesn't follow photo owner
+      if current_user != @photo.owner && @photo.owner.private? && !current_user.leaders.include?(@photo.owner)
+        redirect_back fallback_location: root_url, alert: "Not authorized"
+      end
+    end
+
     # Only allow a list of trusted parameters through.
     def comment_params
       params.require(:comment).permit(:author_id, :photo_id, :body)
     end
-end
+  end
